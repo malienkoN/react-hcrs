ffcb7ac83f0814188ff070cb4e32a0508eac9066
Nikita Malienko <nikita@eltexsoft.com>
2024-11-29 00:59:11 +0100

    fetch data from api

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,7 +1,26 @@
-import { menuData } from "../../data.js";
+import { useEffect, useState } from "react";
 import MenuItem from "../MenuItem/index.jsx";
 
 const Menu = () => {
+    const [menuData, setMenuData] = useState([]);
+
+    useEffect(() => {
+        const fetchMenuData = async () => {
+            try {
+                const res = await fetch(
+                    "https://react-fast-pizza-api.onrender.com/api/menu"
+                );
+                if (!res.ok) throw new Error("Failed to fetch data!");
+                const data = await res.json();
+                setMenuData(data.data);
+            } catch (err) {
+                console.log(err.message);
+            }
+        };
+
+        fetchMenuData();
+    }, []);
+
     return (
         <ul className="menu-container">
             {menuData.map((item) => (
