8fe949c36d067130bfaa96d86b596a09de5fc197
Nikita Malienko <nikita@eltexsoft.com>
2024-12-01 18:40:59 +0100

    added routes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,46 @@
-import CartComponent from "../components/Cart";
+import Button from "../components/Button";
+import CartItem from "../components/CartItem";
+
+const cartItems = [
+    {
+        id: 1,
+        name: "Margherita",
+        price: 12.0,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        name: "Romana",
+        price: 15.0,
+        quantity: 2,
+    },
+    {
+        id: 3,
+        name: "Prosciutto e Rucola",
+        price: 16.0,
+        quantity: 1,
+    },
+];
 
 const Cart = () => {
     return (
-        <>
-            <h1>Cart Page</h1>
-            <CartComponent />
-        </>
+        <div className="container">
+            <a href="/" className="back-link">
+                ← Back to menu
+            </a>
+            <h1 className="cart-title">Your cart</h1>
+
+            <div className="cart-items">
+                {cartItems.map((item) => (
+                    <CartItem item={item} key={item.id} />
+                ))}
+            </div>
+
+            <div className="cart-actions">
+                <Button className="order-btn">Order pizzas</Button>
+                <Button className="clear-btn">Clear cart</Button>
+            </div>
+        </div>
     );
 };
 
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,11 +1,32 @@
-import MenuList from "../components/Menu";
+import { useEffect, useState } from "react";
+import MenuItem from "../components/MenuItem";
 
 const Menu = () => {
+    const [menuData, setMenuData] = useState([]);
+
+    useEffect(() => {
+        const fetchMenuData = async () => {
+            try {
+                const res = await fetch(
+                    "https://react-fast-pizza-api.onrender.com/api/menu"
+                );
+                if (!res.ok) throw new Error("Failed to fetch data!");
+                const data = await res.json();
+                setMenuData(data.data);
+            } catch (err) {
+                console.log(err.message);
+            }
+        };
+
+        fetchMenuData();
+    }, []);
+
     return (
-        <>
-            <h1>Menu Page</h1>
-            <MenuList />
-        </>
+        <ul className="menu-container">
+            {menuData.map((item) => (
+                <MenuItem item={item} key={item.id} />
+            ))}
+        </ul>
     );
 };
 
